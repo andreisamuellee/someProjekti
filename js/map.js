521b811d32fc9fb09d0b7eef2eb62d0ec4f261a2
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2020-12-02 13:38:56 +0200

    Markers from database to map

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYW5kcmVpaCIsImEiOiJja2h2czJrdTUwaTNyMzRtb3JrZjVvYnpmIn0.QGfO5Fq0XigYlW9b89K4aA";
+
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/dark-v10",
@@ -19,26 +20,31 @@ map.addControl(geolocate);
 map.on('load', function () {
   geolocate.trigger();
 });
-var marker = new mapboxgl.Marker()
-  .setLngLat([24.94, 60.19])
-  .addTo(map);
-/*
-  for (let i = 0; i <= result.length; i++) {
-    axios.get("https://api.digitransit.fi/geocoding/v1/search?text="+ result[i].address + ",Helsinki").then(function(response) {
-        var el = document.createElement("div");
-      el.className ="fa fa-map-marker";
-      el.addEventListener('click', () =>
-      {
-        getMenu(result[i])
-      })
-      var hsl = getHslLink(result[i])
-      var googleMaps = getGoogleMapsLink(result[i])
-      new mapboxgl.Marker(el)
-      .setLngLat(response.data.features[0].geometry.coordinates)
-      .setPopup(new mapboxgl.Popup({offset:25}).setHTML('<h2>'+result[i].title+'</h2>' + hsl + googleMaps)).addTo(map);
 
-      }).catch(function (error) {
-        console.log(error);
-      })
+function postsToMap(data) {
+  var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+  mapboxClient.geocoding
+    .forwardGeocode({
+      query: data.Katuosoite + ', ' + data.Paikkakunta,
+      autocomplete: false,
+      limit: 1
+    })
+    .send()
+    .then(function (response) {
+      if (
+        response &&
+        response.body &&
+        response.body.features &&
+        response.body.features.length
+      ) {
+        var feature = response.body.features[0];
+        new mapboxgl.Marker()
+        .setLngLat(feature.center)
+        .setPopup(new mapboxgl.Popup({offset:25}).setHTML(
+          '<h2>'+data.Otsikko+'</h2><p>Posted: '+data.Aikaleima+'</p><p>'+data.Katuosoite + ', ' + data.Paikkakunta+'</p><p>'+data.Tiedot+'</p'
+          ))
+        .addTo(map);
+      }
+    });
 }
-*/
+
